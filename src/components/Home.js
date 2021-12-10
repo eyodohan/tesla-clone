cad2cc064d786ab3ee8bdfcfffbd426f7e8b58c0
eyupcan-dogan <eyodohan@gmail.com>
2021-12-10 12:09:15 +0300

    ADD: Section for the TESLA

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+const Home = () => {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Toucless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Toucless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Toucless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Toucless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lover Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solor Roof Costs Less Than a New Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories"
+        // description="Solor Roof Costs Less Than a New Roof"
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
